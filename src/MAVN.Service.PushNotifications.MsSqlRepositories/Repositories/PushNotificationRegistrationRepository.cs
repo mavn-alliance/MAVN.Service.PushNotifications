0c78b03d39af3e1228e19a28ee4da1cd4adc4a01
anna lizunova <a.lizunova@itransition.com>
2020-05-08 17:06:07 +0300

    Replace LykkeCommon.MsSql package to MAVN.Common.MsSql

--- src/MAVN.Service.PushNotifications.MsSqlRepositories/Repositories/PushNotificationRegistrationRepository.cs
+++ src/MAVN.Service.PushNotifications.MsSqlRepositories/Repositories/PushNotificationRegistrationRepository.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Lykke.Common.MsSql;
+using MAVN.Common.MsSql;
 using MAVN.Service.PushNotifications.Domain;
 using MAVN.Service.PushNotifications.Domain.Enums;
 using MAVN.Service.PushNotifications.Domain.Repositories;
